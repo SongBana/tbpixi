270c2226d8bb2326215ba726633ccda8729ff4f1
BanaSong <904786519@qq.com>
2022-04-02 14:43:19 +0800

    .

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-
-const pixiCanvas = require("./lib/pixi-canvas");
 const pixiSpine = require("./lib/pixi-plugins/pixi-spine");
 const Scroller = require("./lib/scroller");
 
-exports.pixiCanvas = pixiCanvas;
 exports.pixiSpine = pixiSpine;
 exports.Scroller = Scroller;
